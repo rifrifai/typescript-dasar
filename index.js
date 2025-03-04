e0e4003a0c127eb471fccccf7dbc0a2677652981
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2025-03-04 09:35:42 +0700

    function

--- index.js
+++ index.js
@@ -22,4 +22,20 @@ let santri = {
 };
 santri.alamat = "Bandung";
 santri.isLulus = true;
-console.log(santri);
+// tipe data array of object
+let book = {
+    id: 1,
+    item: "Buku",
+    harga: 75000,
+    stock: 10,
+};
+let pen = { id: 2, item: "Pena", harga: 20000, stock: 10 };
+let pensil = { id: 3, item: "Pensil", harga: 5000 };
+// readyonly berfungsi agar tidak bisa diubah
+let items = [book, pen, pensil];
+items[1].harga = 3000;
+//  membuat function di ts
+function hitung(a, b) {
+    return a * b;
+}
+console.log(hitung(3, 3));
--- index.ts
+++ index.ts
@@ -32,3 +32,25 @@ santri.alamat = "Bandung";
 santri.isLulus = true;
 
 // tipe data array of object
+let book: { id: number; item: string; harga: number; stock: number } = {
+  id: 1,
+  item: "Buku",
+  harga: 75000,
+  stock: 10,
+};
+let pen = { id: 2, item: "Pena", harga: 20000, stock: 10 };
+let pensil = { id: 3, item: "Pensil", harga: 5000 };
+// readyonly berfungsi agar tidak bisa diubah
+let items: {
+  readonly id: number;
+  readonly item: string;
+  harga: number;
+  stock?: number;
+}[] = [book, pen, pensil];
+items[1].harga = 3000;
+
+//  membuat function di ts
+function hitung(a: number, b: number): number {
+  return a * b;
+}
+console.log(hitung(3, 3));
