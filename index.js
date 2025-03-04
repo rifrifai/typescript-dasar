96a1b030c10e9e9111c7b30aacf8c721a60b3386
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2025-03-04 07:56:00 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 "use strict";
+// tipe data in ts
 let nama = "Alfian";
-console.log(nama);
+let panjang = nama.length;
+let isCompleted = false;
+let image = null;
+let kosong;
+// union atau mempunyai 2 tipe data
+let link = "https://alfian.com";
+// tipe data array
+let nilai = [90, 80, 65];
+let alamat = ["Jakarta", "Bandung", "Bogor"];
+let mix = ["Budi", 24, "Semarang"];
+nilai.push(22);
+let arrayKosong = [];
+console.log(nilai);
+console.log(arrayKosong);
--- index.ts
+++ index.ts
@@ -1,2 +1,18 @@
+// tipe data in ts
 let nama: string = "Alfian";
-console.log(nama);
+let panjang: number = nama.length;
+let isCompleted: boolean = false;
+let image = null;
+let kosong: undefined;
+
+// union atau mempunyai 2 tipe data
+let link: string | null = "https://alfian.com";
+
+// tipe data array
+let nilai: number[] = [90, 80, 65];
+let alamat: string[] = ["Jakarta", "Bandung", "Bogor"];
+let mix: (string | number)[] = ["Budi", 24, "Semarang"];
+nilai.push(22);
+let arrayKosong: [] = [];
+
+// tipe data object
